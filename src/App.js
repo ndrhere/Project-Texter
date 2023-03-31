1b624aa23c4643b5ba38949029f61b226e682c09
ndrhere <ndx199@gmail.com>
2023-04-01 00:18:53 +0530

    Text application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react'
+import Navbar from './Component/Navbar';
+import Textform from './Component/Textform';
+import { useState } from 'react';
+import Alert from './Component/Alert';
+
+const App = () => {
+const [mode, setMode]= useState('light')
+const [alert, setAlert]= useState(null)
+
+
+
+const showAlert =(message, type)=> {
+   setAlert({
+    msg:message,
+    type:type
+   });
+   setTimeout(()=>{
+    setAlert(null)
+   }, 2000)
+   
 }
 
-export default App;
+
+const toggleMode= ()=>{
+  if(mode==='light'){
+    setMode('dark')
+     document.body.style.backgroundColor= 'rgba(52,53,65,1)'
+     showAlert( 'Dark Mode has been enabled', 'success')
+    
+  }else{
+    setMode('light')
+    document.body.style.backgroundColor= 'white'
+    showAlert('Light Mode has been enabled', 'success' )
+  }
+  
+}
+
+
+
+
+
+
+  return (
+    <>
+    
+    <Navbar toggleMode={toggleMode}
+      mode={mode}/>
+
+      <Alert alert={alert}
+      />
+     
+      <div className=" container my-3 ">
+         <Textform mode={mode}
+         alert={alert}
+         showAlert={showAlert}/>
+       
+
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
